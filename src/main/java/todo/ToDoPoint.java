ce44de5f9034037656a986425e5c8a36b89d476a
mrfox <alexey.listopadov@gmail.com>
2017-10-30 08:26:44 +0200

    add ToDoPoint class, add jackson

---
+++ src/main/java/todo/ToDoPoint.java
@@ -0,0 +1,38 @@
+package todo;
+
+public class ToDoPoint {
+    private String content;
+    private boolean workTag;
+    private boolean emergencyTag;
+
+    public ToDoPoint(String content, boolean workTag, boolean emergencyTag) {
+        this.content = content;
+        this.workTag = workTag;
+        this.emergencyTag = emergencyTag;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public boolean isWorkTag() {
+        return workTag;
+    }
+
+    public void setWorkTag(boolean workTag) {
+        this.workTag = workTag;
+    }
+
+    public boolean isEmergencyTag() {
+        return emergencyTag;
+    }
+
+    public void setEmergencyTag(boolean emergencyTag) {
+        this.emergencyTag = emergencyTag;
+    }
+
+}
--- src/main/java/todo/ToDoServlet.java
+++ src/main/java/todo/ToDoServlet.java
@@ -1,29 +1,50 @@
 package todo;
 
-import java.io.*;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
-import javax.servlet.*;
-import javax.servlet.http.*;
 
 public class ToDoServlet extends HttpServlet {
-    private List<String> points;
+    private List<ToDoPoint> points;
 
     @Override
     public void init() throws ServletException {
-        points = new ArrayList<String>();
+        points = new ArrayList<ToDoPoint>();
+    }
+
+    @Override
+    protected void doOptions(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+            resp.addHeader("Access-Control-Allow-Origin", "*");
+            resp.addHeader("Access-Control-Allow-Methods", "GET, PUT, POST, OPTIONS, DELETE");
+            resp.addHeader("Access-Control-Allow-Headers", "Content-Type");
+            resp.addHeader("Access-Control-Max-Age", "86400");
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setAttribute("points",points);
-        req.getRequestDispatcher("/todo-list.jsp").forward(req,resp);
+        resp.setHeader("Access-Control-Allow-Origin", "*");
+        ObjectMapper mapper = new ObjectMapper();
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("utf-8");
+        PrintWriter out = resp.getWriter();
+        out.print(mapper.writeValueAsString(points));
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        points.add(req.getParameter("point"));
-        resp.sendRedirect("/");
+        resp.setHeader("Access-Control-Allow-Origin", "*");
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode todo = mapper.readValue(req.getReader().readLine(), JsonNode.class);
+        points.add(new ToDoPoint(todo.get("content").asText(),todo.get("workTag").asBoolean(),todo.get("emergencyTag").asBoolean()));
     }
 
 }
