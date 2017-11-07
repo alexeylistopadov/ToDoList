32efbba00dbe99889b74d52c33534d6fd62f705e
mrfox <alexey.listopadov@gmail.com>
2017-11-08 00:15:38 +0200

    Implement getById and update methods

--- src/main/java/todo/TodoService.java
+++ src/main/java/todo/TodoService.java
@@ -14,19 +14,32 @@ public class TodoService {
     public TodoService() {
     }
 
-    public List<TodoPoint> getPoints() {
-        return Collections.unmodifiableList(points);
-    }
-
     public void addPoint(TodoPoint todoPoint) {
         todoPoint.setId(nextId++);
         points.add(todoPoint);
     }
 
-    public void deletePoint(Long pointId) {
-        points = points
-                .stream()
-                .filter(todoPoint -> !todoPoint.getId().equals(pointId))
+    public TodoPoint getPointById(Long id) {
+        return points.stream()
+                .filter(todoPoint -> todoPoint.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public List<TodoPoint> getPoints() {
+        return Collections.unmodifiableList(points);
+    }
+
+    public void updatePoint(TodoPoint point){
+        points.stream()
+                .filter(todoPoint -> todoPoint.getId().equals(point.getId()))
+                .findFirst()
+                .ifPresent(p -> points.set(points.indexOf(p), point));
+    }
+
+    public void deletePoint(Long id) {
+        points = points.stream()
+                .filter(todoPoint -> !todoPoint.getId().equals(id))
                 .collect(Collectors.toList());
     }
 
--- src/main/java/todo/TodoServlet.java
+++ src/main/java/todo/TodoServlet.java
@@ -2,18 +2,12 @@ package todo;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import javax.jws.WebService;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
 
 @WebServlet(name = "todos", urlPatterns = {"/api/todos/*"})
 public class TodoServlet extends HttpServlet {
@@ -30,7 +24,16 @@ public class TodoServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType(APPLICATION_JSON);
-        mapper.writeValue(resp.getWriter(), todoService.getPoints());
+        if (req.getPathInfo() == null) {
+            mapper.writeValue(resp.getWriter(), todoService.getPoints());
+            return;
+        }
+        try {
+            TodoPoint todoPoint = todoService.getPointById(Long.parseLong(req.getPathInfo().substring(1)));
+            mapper.writeValue(resp.getWriter(), todoPoint);
+        } catch (NumberFormatException e) {
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        }
     }
 
     @Override
@@ -38,22 +41,30 @@ public class TodoServlet extends HttpServlet {
         resp.setContentType(APPLICATION_JSON);
         TodoPoint point = mapper.readValue(req.getInputStream(), TodoPoint.class);
         todoService.addPoint(point);
-        mapper.writeValue(resp.getWriter(), point);
     }
 
     @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType(APPLICATION_JSON);
+        todoService.updatePoint(mapper.readValue(req.getInputStream(), TodoPoint.class));
+        resp.setStatus(HttpServletResponse.SC_ACCEPTED);
+    }
+
+
+    @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        if (req.getPathInfo() == null){
+        if (req.getPathInfo() == null) {
             resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
             return;
         }
-
         try {
             todoService.deletePoint(Long.parseLong(req.getPathInfo().substring(1)));
             resp.setStatus(HttpServletResponse.SC_NO_CONTENT);
-        }catch (NumberFormatException e){
+        } catch (NumberFormatException e) {
             resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
         }
     }
+
+
 }
 
